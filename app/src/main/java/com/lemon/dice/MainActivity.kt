bf1a0715ac38d2b8f64952e0e2007c43628cef05
yixin2521 <1emonayu2521@gmail.com>
2024-09-30 17:17:01 +0800

    v2roll

--- app/src/main/java/com/lemon/dice/MainActivity.kt
+++ app/src/main/java/com/lemon/dice/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lemon.dice
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+
+    fun Roll(v: View){
+        var t:TextView=findViewById(R.id.txv)
+        t.text=(1..6).random().toString()
     }
 }
